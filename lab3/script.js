dc0cd82e535cb563ec5688a639f1338a8fc80f86
maciej <mb@4sys.pl>
2023-01-17 21:01:14 +0100

    added channels

--- lab3/script.js
+++ lab3/script.js
@@ -12,43 +12,132 @@ const KeyToSound = {
   'h': document.querySelector('#s9')
 }
 
-const recordButton = document.querySelector('.record-button')
-const playButton = document.querySelector('.play-button')
-let recordingStartTime
-let songNotes
+const recordButtons = document.querySelectorAll('.record-button')
+const playButtons = document.querySelectorAll('.play-button')
+const playSelectedButton = document.querySelector('.play-all')
+const channelNames = document.querySelectorAll('.channel-txt')
+const metronomeBtn = document.getElementById('metronome-btn')
+const channels = {}
+let selectedChannel
+let tempo = 128
+let interval = 60000 / tempo;
+let isMetronomeActive = false;
+let metronomeIntervalId
 
 document.addEventListener('keypress', onKeyPress)
-recordButton.addEventListener('click', toggleRecording)
-playButton.addEventListener('click', togglePlaying)
+playSelectedButton.addEventListener('click', playSelected)
+metronomeBtn.addEventListener('click', toggleMetronome)
 
-function togglePlaying() {
-  playButton.classList.toggle('active')
+recordButtons.forEach(recordButton => {
+  recordButton.addEventListener('click', toggleRecording)
+})
 
-  playSong()
-}
+playButtons.forEach(playButton => {
+  playButton.addEventListener('click', togglePlaying)
+})
 
-function toggleRecording() {
-  recordButton.classList.toggle('active')
+channelNames.forEach(channelName => {
+  channelName.addEventListener('click', toggleSelecting)
+})
 
-  if (isRecording()) {
-    startRecording()
+function toggleMetronome(event) {
+  console.log('toggleMetronome')
+  isMetronomeActive = !isMetronomeActive;
+
+  metronomeBtn.innerHTML = isMetronomeActive ? "Stop" : "Start"
+
+  if (isMetronomeActive) {
+    if (typeof metronomeIntervalId !== 'undefined') clearInterval(metronomeIntervalId);
+    tempo = document.getElementById("tempo").value
+    interval = 60000 / tempo;
+    metronomeIntervalId = setInterval(beat, interval);
   } else {
-    stopRecording()
+    clearInterval(metronomeIntervalId);
   }
 }
 
-function startRecording() {
-  recordingStartTime = Date.now()
-  songNotes = [];
+function beat() {
+  let sound = document.querySelector('#s7')
+
+  if (sound) {
+    sound.currentTime = 0
+    sound.play()
+  }
 }
 
-function stopRecording() {
-  playSong()
+function playSelected() {
+  for (let channel in channels) {
+    if (channels[channel].selected) {
+      playSong(channel)
+    }
+  }
+}
+function togglePlaying(event) {
+  if (isAnyOtherChannelActive(event.target.id)) {
+    return
+  }
+
+  playSong(event.target.id)
 }
 
-function playSong() {
-  if (!songNotes || songNotes.length === 0) return
-  songNotes.forEach(note => {
+function toggleRecording(event) {
+  if (isAnyOtherChannelActive(event.target.id)) {
+    return
+  }
+
+  event.target.classList.toggle('active')
+
+  if (isRecording()) {
+    startRecording(event.target.id)
+  } else {
+    stopRecording(event.target.id)
+  }
+}
+
+function toggleSelecting(event) {
+  let channel = event.target.id
+  event.target.classList.toggle('active')
+
+  if (!channels[channel]) {
+    channels[channel] = {
+      selected: false
+    }
+  }
+
+  channels[channel].selected = !channels[channel].selected
+}
+
+function isAnyOtherChannelActive(channel) {
+  let result = false
+  recordButtons.forEach(recordButton => {
+    if (recordButton.classList.contains('active') && recordButton.id !== channel) {
+      result = true
+    }
+  })
+
+  return result
+}
+
+function startRecording(channel) {
+  if (!channels[channel]) {
+    channels[channel] = {
+      selected: false
+    }
+  }
+
+  channels[channel].start = Date.now()
+  channels[channel].notes = []
+  selectedChannel = channel;
+}
+
+function stopRecording(channel) {
+  selectedChannel = '';
+}
+
+function playSong(channel) {
+  let notes = channels[channel].notes
+  if (!notes || notes.length === 0) return
+  notes.forEach(note => {
     setTimeout(() => {
       playSound(note.key)
     }, note.startTime)
@@ -56,7 +145,18 @@ function playSong() {
 }
 
 function isRecording() {
-  return recordButton != null && recordButton.classList.contains('active')
+  let isRecording = false
+  if (recordButtons == null) {
+    return false
+  }
+
+  recordButtons.forEach(recordButton => {
+    if (recordButton.classList.contains('active')) {
+      isRecording = true
+    }
+  })
+
+  return isRecording
 }
 
 function onKeyPress(event) {
@@ -76,8 +176,13 @@ function playSound(keyCode) {
 }
 
 function recordNote(pressedKey) {
-  songNotes.push({
+  if (!selectedChannel) {
+    return
+  }
+
+  let channel = channels[selectedChannel]
+  channel.notes.push({
     key: pressedKey,
-    startTime: Date.now() - recordingStartTime
+    startTime: Date.now() - channel.start
   })
 }
